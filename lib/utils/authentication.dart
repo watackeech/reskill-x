d489cad53fc271104ebd73175db19b0fd026b95c
watackeech <devkeech@gmail.com>
2023-08-08 11:26:36 +0900

    週間目標の投稿機能を追加

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -29,5 +29,4 @@ class Authentication {
       return false;
     }
   }
-
 }
--- lib/view/goal_screans/goal_screen.dart
+++ lib/view/goal_screans/goal_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:reskill_x/component/main_button.dart';
 import 'package:reskill_x/component/number_scroll.dart';
+import 'package:reskill_x/constant/colors.dart';
+import 'package:reskill_x/model/weekly_plan.dart';
+import 'package:reskill_x/view/screen_control.dart';
 
-class GoalScreen extends StatelessWidget {
+import '../../model/account.dart';
+import '../../utils/authentication.dart';
+import '../../utils/firestore/weekly_plan_firestore.dart';
+
+class GoalScreen extends StatefulWidget {
   const GoalScreen({super.key});
+
+  @override
+  State<GoalScreen> createState() => _GoalScreenState();
+}
+
+class _GoalScreenState extends State<GoalScreen> {
+  Account myAccount = Authentication.myAccount!;
+  int _selectedHour = 1;
+  // WeeklyPlan _weeklyPlan = WeeklyPlan(accountId: 'tanaka@gmail.com', hour: 1, );
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +31,44 @@ class GoalScreen extends StatelessWidget {
       ),
       body: Center(
         child:
-          NumberScroll(
-              upperText: '目標時間を設定してください',
-              buttonText: '目標確定'
+          Column(
+            children: [
+              // NumberScroll(
+              //     upperText: '目標時間を設定してください',
+              //     buttonText: '目標確定',
+              //     onPressed: (int selectedHour) async {
+              //       print('Firebase処理に入ります。');
+              //       WeeklyPlan newWeeklyPlan = WeeklyPlan(
+              //           accountId: myAccount.userId,
+              //           hour: selectedHour
+              //       );
+              //       var result = await WeeklyPlanFirestore.addWeeklyPlan(newWeeklyPlan);
+              //       if(result == true) {
+              //         Navigator.push(context, MaterialPageRoute(builder: (context) => ScreenControl()));
+              //       }
+              //     },
+              // ),
+              NumberPicker(
+                //数字選択するやつ
+                value: _selectedHour,
+                minValue: 0,
+                maxValue: 100,
+                onChanged: (value) => setState(() => _selectedHour = value),
+              ),
+              MainButton(buttonColor: kPrime, buttonTitle: '目標確定',
+                  onTapped: () async {
+                    print('Firebase処理に入ります。');
+                    WeeklyPlan newWeeklyPlan = WeeklyPlan(
+                        accountId: myAccount.id,
+                        hour: _selectedHour
+                    );
+                    var result = await WeeklyPlanFirestore.addWeeklyPlan(newWeeklyPlan);
+                    if(result == true) {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ScreenControl()));
+                    }
+                  }
+              )
+            ],
           ),
       ),
     );
