7be18957c5ac291a45fb2efd311baf34b31ce5fa
tsumugi <tsumushukatsu@gmail.com>
2023-08-14 13:10:46 +0900

    目標設定フォームの内容を修正

--- lib/view/set_goal_form_screen.dart
+++ lib/view/set_goal_form_screen.dart
@@ -3,9 +3,11 @@ import 'package:reskill_x/constant/colors.dart';
 import '../component/main_button.dart';
 import '../component/text_input_field.dart';
 import 'goal_screans/goal_screen.dart';
+
 void main() {
   runApp(SetGoalFormScreen());
 }
+
 class SetGoalFormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,9 +19,19 @@ class SetGoalFormScreen extends StatelessWidget {
     );
   }
 }
-class MyHomePage extends StatelessWidget {
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController inputController1 = TextEditingController();
   final TextEditingController inputController2 = TextEditingController();
+  bool isIntroductionChecked = false;
+  bool isInterestChecked = false;
+  bool isQuestionChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,48 +39,113 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.7,
+            height: MediaQuery.of(context).size.height * 0.75,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  '目標設定面談を実施して、\n設定した目標を入力してください。',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 30),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 40.0),
-                    child: Text(
-                      'あなたは今月何を勉強しますか？\n（例: エクセルのマクロ、Python）',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 16),
-                    ),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '目標設定面談を実施してください\n（所要時間15分)。',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              value: isIntroductionChecked,
+                              onChanged: (bool? newValue) {
+                                setState(() {
+                                  isIntroductionChecked = newValue ?? false;
+                                });
+                              },
+                            ),
+                            Text(
+                              '自己紹介をしてください',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // 以下の部分を追加
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              value: isInterestChecked,
+                              onChanged: (bool? newValue) {
+                                setState(() {
+                                  isInterestChecked = newValue ?? false;
+                                });
+                              },
+                            ),
+                            Text(
+                              '興味分野を共有してください',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'あなたは今月何を勉強しますか？\n（例: エクセルのマクロ、Python）',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      TextInputField(
+                        controller: inputController1,
+                        icon: Icons.book,
+                        label: '勉強内容',
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'あなたは今月どうやって勉強しますか？\n（例: 参考書、Udemy）',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      TextInputField(
+                        controller: inputController2,
+                        icon: Icons.school,
+                        label: '勉強方法',
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              value: isQuestionChecked,
+                              onChanged: (bool? newValue) {
+                                setState(() {
+                                  isQuestionChecked = newValue ?? false;
+                                });
+                              },
+                            ),
+                            Text(
+                              '相手の月間目標に対してコメント・質問\nをしてください',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                TextInputField(
-                  controller: inputController1,
-                  icon: Icons.book,
-                  label: '勉強内容',
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 40.0),
-                    child: Text(
-                      'あなたは今月どうやって勉強しますか？\n（例: 参考書、Udemy）',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ),
-                TextInputField(
-                  controller: inputController2,
-                  icon: Icons.school,
-                  label: '勉強方法',
-                ),
+                SizedBox(height: 40,),
                 MainButton(
                   buttonColor: kPrime,
                   buttonTitle: '送信',
@@ -79,7 +156,10 @@ class MyHomePage extends StatelessWidget {
                     print('入力フォーム2の値: $text2');
                     inputController1.clear();
                     inputController2.clear();
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => GoalScreen()));
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GoalScreen()),
+                    );
                   },
                   textStyle: TextStyle(color: Colors.white),
                 ),
